f3a2d018531f6692bdf1e57bdc365655c670728a
coder-kk <zhangwankang807522215@gmail.comm>
2018-06-30 18:09:22 +0800

    modify order pages

--- pages/orderDetails/orderDetails.js
+++ pages/orderDetails/orderDetails.js
@@ -1,33 +1,60 @@
+
+const app = getApp();
+
 Page({
   data: {
-    orderNum: 123456789,
-    dineNum: 5,
-    tableNum: 3,
-    orderTime: '2017-1-16 11:42',
-    hasData: true,
-    navTab: ["全部", "已支付", "待支付", "已完成", "待处理"],
-    moneyInfo: [100,100,100,100,100,100,100,100],
-    nickName: '薄凉竟是德',
-    phoneNum: '12345678910',
-    url: '',
-    statusText: ['待支付'],
+    orderInfo: null,
+    foods: [],
+    cartDict: []
   },
   onLoad: function(options) {
-    //页面初始化 options为页面跳转所带来的参数
-    wx.setNavigationBarTitle({
-        title: '订单详情',
-    })
+
   },
   onReady:function() {
 
   },
   onShow:function() {
-
+    this.setData({
+      orderInfo: app.globalData.detailOrderInfo
+    })
+    this.splitFood();
+    console.log(this.data.orderInfo);
   },
-  onHide:function() {
-
+  splitFood: function() {
+    let foods = [];
+    let cartDict = [];
+    const originFood = this.data.orderInfo.order.foods;
+    originFood.map(x => {
+      const temp = x.split(", ");
+      foods.push(temp);
+      cartDict.push({
+        count: parseInt(temp[2]),
+        food: {
+          name: temp[0],
+          price: temp[1],
+          image: temp[3]
+        }
+      })
+    })
+    this.setData({
+      foods: foods,
+      cartDict: cartDict
+    })
+    console.log(cartDict);
   },
-  onUnload:function() {
-      
+  touchMerchant: function(e) {
+    console.log(e.currentTarget.dataset.merchantIdx);
+    app.globalData.merchantInfo = this.data.merchants[e.currentTarget.dataset.merchantIdx];
+    wx.navigateTo({
+      url: "../merchantDetails/merchantDetails"
+    })
+  },
+  touchUnpay: function(e) {
+    if(!e.currentTarget.dataset.state) {
+      app.globalData.cartList = this.data.cartDict;
+      wx.navigateTo({
+        url: '../payment/payment'
+      })
+    }
   }
 })
